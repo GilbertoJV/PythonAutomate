86ef0cf0c65c134f9b33b671f73d3e808046fc3e
Gilberto JV <skirlo123aa@gmail.com>
2022-10-16 19:43:51 -0500

    Send email with image

--- automate_email.py
+++ automate_email.py
@@ -2,6 +2,7 @@ import os
 from email.message import EmailMessage
 import ssl
 import smtplib
+import imghdr
 from dotenv import load_dotenv
 
 # take env variables from a .env file
@@ -18,12 +19,19 @@ Creado por: Gilberto JV Skirlo.
 Saludos.
 """
 
+#Add img to email
+with open('Firma.png','rb') as file:
+  file_data = file.read()
+  file_type = imghdr.what(file.name)
+  file_name = file.name
+
 #Make email instance
 em = EmailMessage()
 em['From'] = os.environ['email_user']
 em['To'] = email_receiver
-em['Subject'] = 'Correo Automatizado'
+em['Subject'] = 'Correo Automatizado 2'
 em.set_content(email_body)
+em.add_attachment(file_data, filename=file_name, subtype=file_type, maintype='image')
 
 #add ssl certificate
 context = ssl.create_default_context()
